962798b83ac4b2cb83cf7f4a9593478054eb1bfc
Sanskar Amgain <sanskar.amgain@lisnepal.com.np>
2022-11-18 19:16:02 +0545

    setup created

--- core/core.go
+++ core/core.go
@@ -1,75 +1,21 @@
 package core
 
+import "GuthiNetwork/shm"
+
+//TODO: Need better name
 /*
-#cgo CXXFLAGS: "-std=c++20 -I../Guthi-Core/src/"
-#cgo LDFLAGS: -L../Guthi-Core/lib -lGuthiCore -lstdc++
-#include "../Guthi-Core/src/core/c_api.h"
+	Initializes the the basic structure needed for core communication
 */
-import "C"
-import "fmt"
-
-func Initialize() interface{} {
-	GetSysMemoryInfo()
-	fmt.Println(GetCPUAllUsage())
-	fmt.Println(GetCPUAllUsage())
-	return GetProcessorInfo()
-}
-
-// Runtime info structure
-
-// ------------------CPU----------------------
-type ProcessorInfo struct {
-	Processor_number uint32
-	Current_mhz      uint32
-	Total_mhz        uint32
-}
-
-type ProcessorStatus struct {
-	Processor_count uint32
-	Processors      []ProcessorInfo
-}
-
-func GetProcessorInfo() ProcessorStatus {
-	info := C.GetSysProcessorInfo()
-	status := ProcessorStatus{
-		uint32(info.processor_count),
-		[]ProcessorInfo{},
+func Initialize() error {
+	var err error
+	shared_memory, err = shm.CreateSharedMemory()
+	if err != nil {
+		return err
 	}
-	for i := 0; i < int(status.Processor_count); i++ {
-		info := ProcessorInfo{
-			uint32(info.processors[i].processor_number),
-			uint32(info.processors[i].current_mhz),
-			uint32(info.processors[i].total_mhz),
-		}
-		status.Processors = append(status.Processors, info)
+	semaphore, err = shm.CreateSemaphore()
+	if err != nil {
+		return err
 	}
 
-	return status
-}
-
-/*
-Returns total CPU usage in percentage
-*/
-func GetCPUAllUsage() float64 {
-	usage := C.GetCurrentAllCPUUsage()
-	return float64(usage)
-}
-
-// ------------------Memory--------------
-type MemoryStatus struct {
-	Installed_physical_ram uint64 `json:"installed"`
-	Available_ram          uint64 `json:"Available"`
-	Memory_load            uint64 `json:"Memory_Load"`
-	// Information about virtual memory is not required here
-}
-
-func GetSysMemoryInfo() MemoryStatus {
-	info := C.GetSysMemoryInfo()
-	memory_status := MemoryStatus{
-		uint64(info.installed_physical_ram),
-		uint64(info.available_ram),
-		uint64(info.memory_load),
-	}
-
-	return memory_status
+	return nil
 }
---
+++ setup.sh
@@ -0,0 +1,6 @@
+git submodule update --init
+git submodule update --remote  # update from remote
+cd Guthi-Core
+cmake CMakeLists.txt
+make
+cd ..
